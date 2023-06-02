34bd7662e75d29c16b6be07409a44acf82842fee
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-06-02 21:57:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function threeSum(arr, target) {
 // write your code here
+	// write your code here
+function decimalToBinary(num) {
+    let res = [];
+    while (num > 0) {
+        res.push(num % 2);
+        num = Math.floor(num / 2);
+    }
+    return res.reverse().join('');
+}
+console.log(decimalToBinary(7)); // Output: 111
+console.log(decimalToBinary(10)); // Output: 1010
+console.log(decimalToBinary(33)); // Output: 100001
+
   
 }
 
