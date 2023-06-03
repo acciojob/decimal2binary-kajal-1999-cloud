a262cf88708f3aa0c314ee6bfc312aa32e1fd1de
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-06-03 13:28:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-// write your code here
-function decimalToBinary(num) {
-    let res = [];
-    while (num > 0) {
-        res.push(num % 2);
-        num = Math.floor(num / 2);
-    }
-    return res.reverse().join('');
-}
-console.log(decimalToBinary(7)); // Output: 111
-console.log(decimalToBinary(10)); // Output: 1010
-console.log(decimalToBinary(33)); // Output: 100001
+function decimalToBinary(decimal) {
+  // Initialize an empty binary string
+  let binary = '';
 
-}
-module.export = num ;
-// module.exports = threeSum;
+  // Convert decimal to binary
+  while (decimal > 0) {
+    // Get the remainder when dividing by 2
+    let remainder = decimal % 2;
+
+    // Add the remainder to the binary string
+    binary = remainder + binary;
+
+    // Update the decimal by dividing by 2
+    decimal = Math.floor(decimal / 2);
+  }
+
+  // Return the binary representation
+  return binary;
+
