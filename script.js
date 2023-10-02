2ad767bad53b8248587a529f6d2c5c7a2a242a3a
kajal-1999-cloud <130256390+kajal-1999-cloud@users.noreply.github.com>
2023-10-02 20:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function decimalToBinary(num) {
-  // Write your code here
+  if (num < 0 || !Number.isInteger(num)) {
+    return 'Invalid input. Please enter a non-negative integer.';
+  }
+
+  if (num === 0) {
+    return '0';
+  }
+
+  let binary = '';
+  while (num > 0) {
+    binary = (num % 2) + binary;
+    num = Math.floor(num / 2);
+  }
+
+  return binary;
 }
 
 window.decimalToBinary = decimalToBinary;
+
